aaa1e360ec936741ed6ac6945cce60460535f943
Danilo Rodrigues de Oliveira <danilo.rodrigues38@gmail.com>
2023-03-07 20:03:05 -0300

    Create tabuada.js

---
+++ Aulas Front-End HTML5/2-SEGUNDA/Aula 02/modulo/tabuada.js
@@ -0,0 +1,13 @@
+/******************************************************************************************
+ * Objetivo: Realizar o caulculo de uma tabuada.
+ * Autor: Danilo Rodrigues de Oliveira
+ * Data: 07/03/2023
+ * Versão: 0.0.1 
+ *****************************************************************************************/
+
+
+function tabuada(num){
+    for (var cont = 1; cont < 10; cont++){
+        console.write(num + " * " + cont + " = " + (num * cont))
+    }
+}
