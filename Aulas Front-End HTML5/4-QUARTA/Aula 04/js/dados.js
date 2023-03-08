fba08cbca590937d1250d51aba8e4677bb277581
Danilo Rodrigues de Oliveira <danilo.rodrigues38@gmail.com>
2023-03-08 20:17:14 -0300

    Create dados.js

---
+++ Aulas Front-End HTML5/4-QUARTA/Aula 04/js/dados.js
@@ -0,0 +1,17 @@
+/******************************************************************************************
+ * Objetivo: Manipular elementos HTML pelo JavaScript.
+ * Autor: Danilo Rodrigues de Oliveira
+ * Data: 08/03/2023
+ * Versão: 0.0.1 
+ *****************************************************************************************/
+
+// Criamos um objeto que recebe todas as características do botão.
+var botaoTabela = document.getElementById("tabela");
+
+const criarElementos = function() {
+    alert("teste");
+};
+
+botaoTabela.addEventListener("click", function(){
+    criarElementos();
+});
