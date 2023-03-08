04690691a97ebe74e40b707a1952a8c039f7c7bd
Danilo Rodrigues de Oliveira <danilo.rodrigues38@gmail.com>
2023-03-07 21:21:39 -0300

    Update tabuada.js

--- Aulas Front-End HTML5/2-SEGUNDA/Aula 02/modulo/tabuada.js
+++ Aulas Front-End HTML5/2-SEGUNDA/Aula 02/modulo/tabuada.js
@@ -5,9 +5,19 @@
  * Versão: 0.0.1 
  *****************************************************************************************/
 
+// Gerar a tabuada de um número.
+const calcularTabuada = function(numeroTabuada){
+    let tabuada = numeroTabuada;
 
-function tabuada(num){
-    for (var cont = 1; cont < 10; cont++){
-        console.write(num + " * " + cont + " = " + (num * cont))
+    if (isNaN(tabuada) || tabuada == 0){
+        console.log("Erro! Digite apenas números diferentes de 0 (zero).");
+    } else {
+        for (cont = 1; cont <= 10; cont++){
+            console.log(`${tabuada} X ${cont.toString().padStart(2, ' ')} = ${(tabuada * cont).toString().padStart(2, ' ')}`);
+        }
     }
+    return status;
 }
+
+calcularTabuada(5);
+
