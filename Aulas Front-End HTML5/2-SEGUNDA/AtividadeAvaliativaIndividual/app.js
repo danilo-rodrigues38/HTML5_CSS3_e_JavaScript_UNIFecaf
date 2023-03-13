a0b34544588f15ffb1fee08b5bb563f6f996b572
Danilo Rodrigues de Oliveira <danilo.rodrigues38@gmail.com>
2023-03-13 19:28:56 -0300

    Update app.js

--- Aulas Front-End HTML5/2-SEGUNDA/AtividadeAvaliativaIndividual/app.js
+++ Aulas Front-End HTML5/2-SEGUNDA/AtividadeAvaliativaIndividual/app.js
@@ -1,6 +1,6 @@
 // Importe dos arquivos e funções.
 var readline = require('readline');
-var somatorio = require('./modulo/somar.js');
+var matematica = require('./modulo/somar.js');
 
 var teclado = readline.createInterface({
     input : process.stdin,
@@ -15,4 +15,4 @@ teclado.question("Digite outro número: ",function(num2){
     var numero2 = num2;
 })
 
-document.write("A soma entre " + numero1 + " e " + numero2 + " è igual a " + somatorio.somar(numero1, numero2));
+console.log("A soma entre " + numero1 + " e " + numero2 + " è igual a " + matematica.somar(numero1, numero2));
