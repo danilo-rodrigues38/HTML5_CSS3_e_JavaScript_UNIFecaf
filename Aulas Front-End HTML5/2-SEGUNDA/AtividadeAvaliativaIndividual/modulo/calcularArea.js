146641d328418571a7e29598d9aef5bc73e63f4d
Danilo Rodrigues de Oliveira <danilo.rodrigues38@gmail.com>
2023-03-13 20:54:45 -0300

    Update calcularArea.js

--- Aulas Front-End HTML5/2-SEGUNDA/AtividadeAvaliativaIndividual/modulo/calcularArea.js
+++ Aulas Front-End HTML5/2-SEGUNDA/AtividadeAvaliativaIndividual/modulo/calcularArea.js
@@ -1,33 +1,33 @@
 /******************************************************************************************
- * Objetivo: Realizar o claculo de área de um quadrado e um círculo.
+ * Objetivo: Realizar o calculo de área de um quadrado e um círculo.
  * Autor: Danilo Rodrigues de Oliveira
  * Data: 13/03/2023
  * Versão: 0.0.1 
  *****************************************************************************************/
 
-const getCalculoQuadrado = function(base, altura){
-    let base = base;
-    let altura = altura;
+const getCalculoQuadrado = function(vlbase, vlaltura){
+    let base = vlbase;
+    let altura = vlaltura;
     let status = true;
 
     if (isNaN(base) || isNaN(altura)){
         status = false;
     } else {
-        console.log("A área do quadrado é " + base * altura);
+        console.log("A área do quadrado é " + base * altura + "m².");
     }
     return status;
 };
 
 const getCalculoCirculo = function(num1){
     let raio = num1;
-    let area = 0;
+    let area;
     let status = true;
 
     if(isNaN(raio)){
         status = false;
     } else {
-        area = Math.PI * area * area;
-        console.log("A área do círculo é " + area)
+        area = Math.PI * raio * raio;
+        console.log("A área do círculo é " + area);
     }
     return status;
 };
