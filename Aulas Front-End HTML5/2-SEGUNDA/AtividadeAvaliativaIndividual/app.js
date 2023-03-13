b136dc5e9234c60c771352d7fe1a5f2166950ea1
Danilo Rodrigues de Oliveira <danilo.rodrigues38@gmail.com>
2023-03-13 19:12:48 -0300

    Create app.js

---
+++ Aulas Front-End HTML5/2-SEGUNDA/AtividadeAvaliativaIndividual/app.js
@@ -0,0 +1,19 @@
+// Importe dos arquivos e funções.
+var readline = require("readline");
+const { somar } = require("./modulo/soma");
+var somatorio = require("./modulo/soma");
+
+var teclado = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout
+});
+
+teclado.question("Digite um número: ", function(num1){
+    var numero1 = num1;
+});
+
+teclado.question("Digite outro número: ",function(num2){
+    var numero2 = num2;
+})
+
+document.write("A soma entre " + numero1 + " e " + numero2 + " è igual a " + somatorio.somar(numero1, numero2));
