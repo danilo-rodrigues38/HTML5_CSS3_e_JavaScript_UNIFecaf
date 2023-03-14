a53e1223842df2db42c6b8256e398361fda8d4a3
Danilo Rodrigues de Oliveira <danilo.rodrigues38@gmail.com>
2023-03-13 21:49:16 -0300

    Update app.js

--- Aulas Front-End HTML5/2-SEGUNDA/AtividadeAvaliativaIndividual/app.js
+++ Aulas Front-End HTML5/2-SEGUNDA/AtividadeAvaliativaIndividual/app.js
@@ -8,14 +8,15 @@ var entradaDados = readline.createInterface({
 });
 
 console.log("\n\nCALCULANDO A ÁREA DO CÍRCULO");
-console.log("Raio = 1.25");
-var valorRaio = 1.25;
-matematica.getCalculoCirculo(valorRaio);
+entradaDados.question('Digite um valor para ser calculada a área de um circulo: ', function(vlRaioCirculo) {
+    let vlraio = vlRaioCirculo;
+    matematica.getCalculoCirculo(vlraio)
+});
 
-console.log("\n\nCALCULANDO A ÁREA DO QUADRADO");
-console.log("Base = 5");
-var entradaValorBase = 5;
-console.log("Altura = 3");
-var entradaValorAltura = 3;
+// console.log("\n\nCALCULANDO A ÁREA DO QUADRADO");
+// console.log("Base = 5");
+// var entradaValorBase = 5;
+// console.log("Altura = 3");
+// var entradaValorAltura = 3;
 
-matematica.getCalculoQuadrado(entradaValorBase, entradaValorAltura);
+// matematica.getCalculoQuadrado(entradaValorBase, entradaValorAltura);
