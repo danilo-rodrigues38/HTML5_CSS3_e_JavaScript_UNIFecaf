dbdf6957aad581ea367ed6981dd65209d1b55d26
Danilo Rodrigues de Oliveira <danilo.rodrigues38@gmail.com>
2023-03-13 21:48:58 -0300

    Update tabuada.js

--- Aulas Front-End HTML5/2-SEGUNDA/Aula 02/modulo/tabuada.js
+++ Aulas Front-End HTML5/2-SEGUNDA/Aula 02/modulo/tabuada.js
@@ -6,21 +6,60 @@
  *****************************************************************************************/
 
 // Gerar a tabuada de um número.
+// const calcularTabuada = function(numeroTabuada){
+//     let tabuada = numeroTabuada;
+//     let status = true;
+
+//     if (isNaN(tabuada) || tabuada == 0){
+//         status = false;
+//     } else {
+//         for (cont = 1; cont <= 10; cont++){
+//             console.log(`${tabuada} X ${cont.toString().padStart(2, ' ')} = ${(tabuada * cont).toString().padStart(2, ' ')}`);
+//         }
+//     }
+//     return status;
+// }
+
+// // Para acessar uma função em outro arquivo é necessário fazer o exports da função.
+// module.exports = {
+//     calcularTabuada
+// }
+
+//Gerar a tabuada de um número
 const calcularTabuada = function(numeroTabuada){
+    //Entradas
     let tabuada = numeroTabuada;
-    let status = true;
+    let status;
+    let cont = 0;
+    let resultado;
 
-    if (isNaN(tabuada) || tabuada == 0){
+    
+    //Processamento
+
+    if(tabuada == '')
         status = false;
-    } else {
-        for (cont = 1; cont <= 10; cont++){
-            console.log(`${tabuada} X ${cont.toString().padStart(2, ' ')} = ${(tabuada * cont).toString().padStart(2, ' ')}`);
+    else if(isNaN(tabuada))
+        status = false;
+    else if(tabuada == 0)
+        status = false;
+    else if(tabuada == null)
+        status = false;
+    else{
+        //While
+        while (cont <= 10){
+            resultado = tabuada * cont;
+            console.log(tabuada + 'x' + cont + '=' + resultado);
+            cont +=1;
         }
+        status = true;
     }
-    return status;
-}
 
-// Para acessar uma função em outro arquivo é necessário fazer o exports da função.
+    //Saída
+    return status;
+};
+
+
+//Para acessar uma função em outro arquivo, é necessário fazer o exports da função
 module.exports = {
     calcularTabuada
 }
