90906a44542a4776308b0c3de5a95a0b91f02bf1
Danilo Rodrigues de Oliveira <danilo.rodrigues38@gmail.com>
2023-03-19 20:49:29 -0300

    Create calculadora.js

---
+++ Aulas Front-End HTML5/4-QUARTA/AAI - Danilo Rodrigues de Oliveira - 26836/js/calculadora.js
@@ -0,0 +1,59 @@
+/****************************************************************************
+ * Objetivo: Criar uma calculadora com apenas uma entrada para calculos
+ * Data: 17/03/2023
+ * Autor: Danilo Rodrigues de Oliveira
+ * Versão: 0.0.1
+ *******************************************************************************/
+
+//letiaveis globais
+let numero;
+let calculo="";
+
+//Funções que receber valor digitado e atribuir a uma letiavel
+//Função limpar dados 
+document.getElementById("somar").addEventListener("click",() => { 
+    numero = Number(document.getElementById("valor").value);
+    calculo = "somar";
+    document.getElementById("valor").value="";
+});
+
+document.getElementById("subtrair").addEventListener("click",() => {
+    numero = Number(document.getElementById("valor").value);
+    calculo = "subtrair";
+    document.getElementById("valor").value="";
+});
+
+document.getElementById("dividir").addEventListener("click",() => {
+    numero = Number(document.getElementById("valor").value);
+    calculo = "dividir";
+    document.getElementById("valor").value="";
+});
+
+document.getElementById("multiplicar").addEventListener("click",() => {
+    numero = Number(document.getElementById("valor").value);
+    calculo = "multiplicar";
+    document.getElementById("valor").value="";
+});
+
+document.getElementById('ce').addEventListener("click",() => {
+    document.getElementById("valor").value="";
+    document.getElementById('exibir').innerText="";
+    numero=0;
+    calculo="";
+});
+
+//Função calculo
+document.getElementById("calcular").addEventListener("click",() => {
+    let resultado;
+    if (calculo=="somar"){
+        resultado=numero+Number(document.getElementById("valor").value);
+    } else if (calculo=="subtrair"){
+        resultado=numero-Number(document.getElementById("valor").value);
+    } else if (calculo=="dividir"){
+        resultado=numero/Number(document.getElementById("valor").value);
+    } else if (calculo=="multiplicar"){
+        resultado=numero*Number(document.getElementById("valor").value);
+    }
+    document.getElementById("resultado").innerText=resultado;
+    document.getElementById("valor").value = "";
+});
