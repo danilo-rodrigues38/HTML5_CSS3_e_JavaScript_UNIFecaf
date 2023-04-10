9864f9db25883ac602f1694f7a89c1217671a0c7
Danilo Rodrigues de Oliveira <danilo.rodrigues38@gmail.com>
2023-04-10 20:56:42 -0300

    Remoção da pasta node modules

---
+++ Aulas Front-End HTML5/2-SEGUNDA/Aula 04/app.js
@@ -0,0 +1,8 @@
+/**
+ * Pacotes necessários para criar uma API no node.
+ * npm install express --save
+ * npm install cors --save
+ * npm install body-parser --save
+ * 
+ * Para reinstalar todo o projeto rodar o comando: npm i
+ */
