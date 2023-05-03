7d21eddc9a0a71fe7ef799aa4e66e4654589b4a2
Danilo Rodrigues de Oliveira <danilo.rodrigues38@gmail.com>
2023-05-02 21:45:44 -0300

    Update arquivos

---
+++ Aulas Front-End HTML5/3-TERCA/Aula 09/js/carregarLivros.js
@@ -0,0 +1,13 @@
+/************************************************************************************************
+ * Objetivo: Arquivo responsável por carregar os dados dos livros, fazendo a integração entre o Front e Back
+ * Nome: Danilo Rodrigues de Oliveira
+ * Data: 02/05/2023
+ * Versão: 1.0.0
+ ***********************************************************************************************/
+
+// Retorna todos os livros
+const getLivros = function(){
+    
+}
+
+window.addEventListener('load', function(){ getLivros(); });
