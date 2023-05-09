29cb9e7f5eb06fc6d1cc43b2f432e40b46ef750c
Danilo Rodrigues de Oliveira <danilo.rodrigues38@gmail.com>
2023-05-08 21:52:38 -0300

    Create AlunoController.js

---
+++ Aulas Front-End HTML5/2-SEGUNDA/Aula 05/controller/AlunoController.js
@@ -0,0 +1,33 @@
+/************************************************************************************************
+ * Controller: ALUNO
+ * Objetivo: Arquivo responsável pela criação da regra de negócio da aplicação.
+ * Nome: Danilo Rodrigues de Oliveira.
+ * Data: 08/05/2023.
+ * Versão: 1.0.0
+ ***********************************************************************************************/
+
+// Função para incerir um novo aluno.
+const incerirAluno = function(dadosAluno){
+
+};
+
+// Função para atualizar um aluno.
+const atualizarAluno = function(dadosAluno, id){
+
+};
+
+// Função para excluir um aluno.
+const excluirAluno = function(id){
+
+};
+
+// Função para listar todos os alunos.
+const listarAluno = function(){
+
+};
+
+
+// Função para buscar um aluno pelo ID.
+const buscarAluno = function(id){
+
+};
