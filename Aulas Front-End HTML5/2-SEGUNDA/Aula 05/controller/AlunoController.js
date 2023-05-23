5ce0786e0f88fd1fb40ecd6e29fb3cb2a6a9ca1e
Danilo Rodrigues de Oliveira <danilo.rodrigues38@gmail.com>
2023-05-23 20:39:09 -0300

    Update AlunoController.js

--- Aulas Front-End HTML5/2-SEGUNDA/Aula 05/controller/AlunoController.js
+++ Aulas Front-End HTML5/2-SEGUNDA/Aula 05/controller/AlunoController.js
@@ -6,41 +6,99 @@
  * Versão: 1.0.0
  ***********************************************************************************************/
 
+var alunoDAO = require("../model/DAO/aluno");
+
 // Função para incerir um novo aluno.
-const inserirAluno = function(dadosAluno){
+const inserirAluno = async function(dadosAluno){
+    // Validação de contrúdo obrigatório
     if (dadosAluno.nome     == '' || dadosAluno          == undefined || dadosAluno.nome.length     > 100 ||
         dadosAluno.cpf      == '' || dadosAluno.cpf      == undefined || dadosAluno.cpf.length      > 18  ||
         dadosAluno.email    == '' || dadosAluno.email    == undefined || dadosAluno.email.length    > 256 ||
         dadosAluno.telefone == '' || dadosAluno.telefone == undefined || dadosAluno.telefone.length > 20){
             return {statusCode: 400, message: "Dados obrigatórios não foram preenchidos."}
+    } else {
+        let result = await alunoDAO.insertAluno(dadosAluno);
+        if (result) {
+            return {statusCode: 201, message: "Item inserido com sucesso!"};
         } else {
-            let alunoDAO = require("../model/DAO/aluno");
-            let result = alunoDAO.insertAluno(dadosAluno);
+            return {statusCode: 500, message: "Erro interno do servidor!"}
+        }
+    }
+};
+
+// Função para atualizar um aluno.
+const atualizarAluno = async function(dadosAluno, id){
+    if (dadosAluno.nome     == '' || dadosAluno          == undefined || dadosAluno.nome.length     > 100 ||
+        dadosAluno.cpf      == '' || dadosAluno.cpf      == undefined || dadosAluno.cpf.length      > 18  ||
+        dadosAluno.email    == '' || dadosAluno.email    == undefined || dadosAluno.email.length    > 256 ||
+        dadosAluno.telefone == '' || dadosAluno.telefone == undefined || dadosAluno.telefone.length > 20){
+            return {statusCode: 400, message: "Dados obrigatórios não foram preenchidos."}
+    } else {
+        // Validação do ID inválido
+        if (idAluno == '' || idAluno == undefined || isNaN(idAluno)) {
+            return {statusCode: 404, message: "O ID não foi encontrado ou ID inválido."}
+        } else {
+            // Adiciona um novo atriburo no JSON (id)
+            dadosAluno.id = idAluno;
+            let result = await alunoDAO.updateAluno(dadosAluno);
             if (result) {
                 return {statusCode: 201, message: "Item inserido com sucesso!"};
             } else {
                 return {statusCode: 500, message: "Erro interno do servidor!"}
             }
         }
-};
-
-// Função para atualizar um aluno.
-const atualizarAluno = function(dadosAluno, id){
-
+    }
 };
 
 // Função para excluir um aluno.
-const excluirAluno = function(id){
-
+const excluirAluno = async function(id){
+    if (id == "" || id == undefined || isNaN(id)){
+        return {statusCode: 404, message: "O ID não foi informado ou é inválido."}
+    } else {
+        let result = await alunoDAO.deleteAluno(id);
+        if (result) {
+            return {statusCode: 200, message: "Item removido com sucesso."}
+        } else {
+            return {statusCode: 500, message: "Erro interno na API ou no Servidor do Banco de Dados."}
+        }
+    }
 };
 
 // Função para listar todos os alunos.
-const listarAluno = function(){
+const listarAluno = async function(){
+    // Chama a função que vai buscar no BD os registros
+    let result = await alunoDAO.selectAllAluno();
 
+    // Se houver dados no banco
+    if (result.length > 0) {
+        // Retorna um JSON com os statusCode e o Array de itens.
+        return {statusCode: 200, alunos: result};
+    } else {
+        return {statusCode: 404, message: "Dados não localizados."};
+    }
 };
 
 
 // Função para buscar um aluno pelo ID.
-const buscarAluno = function(id){
-
+const buscarAluno = async function(id){
+    // Validação do ID
+    if (id == "" || id == undefined || isNaN(id)){
+        return {statusCode: 404, message: "O ID não foi informado ou é inválido."}
+    } else {
+        // Chama a função para filtrar no BD pelo ID
+        let result = await alunoDAO.selectByIdAluno(id);
+        if (result.length > 0) {
+            return {statusCode: 200, aluno: result}
+        } else {
+            return {statusCode: 404, message: "Nenhum registro encontrado."}
+        }
+    }
 };
+
+mudule.exports {
+    inserirAluno,
+    atualizarAluno,
+    excluirAluno,
+    listarAluno,
+    buscarAluno
+}
