6b9c6290fb35f580274441fc19ccfd78fc507c36
Danilo Rodrigues de Oliveira <danilo.rodrigues38@gmail.com>
2023-05-27 19:29:49 -0300

    Create instances NPM

---
+++ Aulas Front-End HTML5/2-SEGUNDA/TrabalhoMarcelEstados/app.js
@@ -0,0 +1,15 @@
+/**
+ * Objetivo: API para retornar dados dos Estados
+ * Autor: Danilo Rodrigues de Oliveira
+ * Data: 19/05/2023
+ * Versão: 0.0.1
+ */
+
+/**
+ * Pacotes necessários para criar uma API no node.
+ * npm install express --save
+ * npm install cors --save
+ * npm install body-parser --save
+ * 
+ * Para reinstalar todo o projeto, rodar o comando: npm i
+ */
