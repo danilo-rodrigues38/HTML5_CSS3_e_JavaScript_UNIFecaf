bb52d99da493bcc2408d7adb4c6c2309bac0f8b5
Danilo Rodrigues de Oliveira <danilo.rodrigues38@gmail.com>
2023-05-27 21:12:17 -0300

    Update app.js

--- Aulas Front-End HTML5/2-SEGUNDA/TrabalhoMarcelEstados/app.js
+++ Aulas Front-End HTML5/2-SEGUNDA/TrabalhoMarcelEstados/app.js
@@ -1,6 +1,7 @@
 /**
  * Objetivo: API para retornar dados dos Estados
  * Autor: Danilo Rodrigues de Oliveira
+ *        Kaleo Vieira Leite
  * Data: 19/05/2023
  * Versão: 0.0.1
  */
@@ -13,3 +14,48 @@
  * 
  * Para reinstalar todo o projeto, rodar o comando: npm i
  */
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use((request, response, next) => {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+
+    app.use(cors());
+
+    next();
+});
+
+app.get("/uf", cors(), async function(request, response){
+    let siglaDoEstado = request.query.sigla;
+    let estadosCidades;
+    let estados = require("./modulo/estados_cidades.js");
+
+    if (siglaDoEstado != "" && siglaDoEstado != undefined){
+        estadosCidades = estados.getEstadosNome(siglaDoEstado);
+    } else {
+        estadosCidades = estados.getEstadositem();
+    }
+
+    if (estados) {
+        response.status(200);
+        response.json(estadosCidades);
+    } else {
+        response.status(404);
+        response.json();
+    }
+});
+
+
+
+
+
+
+
+
+app.listen(8080, function(){
+    console.log("Servidor aguardando requisição na porta 8080.");
+});
